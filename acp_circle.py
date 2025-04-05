93846c2b7f483d0af6cccbf27868fa2f2d235f5f
dharsanceo <sdharsan13032014@gmail.com>
2025-04-05 11:55:05 +0530

    Add files via upload

--- acp_circle.py
+++ acp_circle.py
@@ -4,8 +4,8 @@ class Circle():
 
     def circle_area(self):
        return self.radius ** 2
-cr=int(input("Dimension of circle -radius" ))
-newcircle = Circle(cr5)
+
+newcircle = Circle(7)
     
 print("Dimension of circle -radius  %d" % (newcircle.radius))
 print("Area of circle :", newcircle.circle_area())
